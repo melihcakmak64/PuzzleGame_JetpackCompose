f17d89e19ef0a305b71b34b612de9fc7a6ce0d50
Melih Çakmak <melihcakmak1755@gmail.com>
2025-08-08 13:29:18 +0300

    refactoring

--- app/src/main/java/com/solardevtech/puzzle/view/screens/DragAndDropScreen.kt
+++ app/src/main/java/com/solardevtech/puzzle/view/screens/DragAndDropScreen.kt
@@ -2,7 +2,6 @@ package com.solardevtech.puzzle.view.screens
 
 
 import PuzzleBoxList
-import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
@@ -12,40 +11,38 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.solardevtech.puzzle.R
 import com.solardevtech.puzzle.view.components.PuzzleGrid
 import com.solardevtech.puzzle.viewmodel.DragAndDropViewModel
+import androidx.compose.ui.platform.LocalWindowInfo
 
-@SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun DragAndDropWithGridScreen(
     viewModel: DragAndDropViewModel = viewModel()
 ) {
     val density = LocalDensity.current
-
-    // Grid ayarları
-    val boxSizeDp: Dp = 100.dp
-    val gridSizeDp = boxSizeDp * viewModel.cellCountPerRow
     val context = LocalContext.current
+    val windowSizePx = LocalWindowInfo.current.containerSize
+
+    val boxSizeDp = 100.dp
+    val gridSizeDp = boxSizeDp * viewModel.cellCountPerRow
+    val gridSizePx = with(density) { gridSizeDp.toPx() }
+    val cellSizePx = gridSizePx / viewModel.cellCountPerRow
+
+    // Gerçek pencere boyutlarına göre grid'in başlangıç noktası
+    val gridStart = Offset(
+        x = (windowSizePx.width - gridSizePx) / 2f,
+        y = (windowSizePx.height - gridSizePx) / 2f
+    )
 
     LaunchedEffect(Unit) {
         val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.sample)
         viewModel.generateBoxesFromImage(bitmap)
     }
 
-    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-        val gridSizePx = with(density) { gridSizeDp.toPx() }
-        val cellSizePx = gridSizePx / viewModel.cellCountPerRow
-
-        // Grid'i ekranın ortasına yerleştirmek için başlangıç noktası
-        val startX = (constraints.maxWidth - gridSizePx) / 2f
-        val startY = (constraints.maxHeight - gridSizePx) / 2f
-        val gridStart = Offset(startX, startY)
-
-        // Grid ve yerleşmiş kutular
+    Box(modifier = Modifier.fillMaxSize()) {
         PuzzleGrid(
             viewModel = viewModel,
             gridStart = gridStart,
@@ -53,7 +50,6 @@ fun DragAndDropWithGridScreen(
             boxSizeDp = boxSizeDp
         )
 
-        // Alt tarafta kaydırılabilir sürüklenebilir kutular
         Box(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
